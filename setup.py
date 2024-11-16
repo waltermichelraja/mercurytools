cc22115121ef967799180c69dc62b2abf8aaad11
Walter Michel Raja <85942041+waltermichelraja@users.noreply.github.com>
2024-11-16 07:43:46 +0530

    publish commit

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mercuryds",
+    name="mercurydsa",
     version="0.0.2",
-    packages=find_packages(),
     author="Walter Michel Raja",
-    author_email="waltermichelraja@gmail.com"
+    author_email="waltermichelraja@gmail.com",
+    packages=find_packages()
 )
