b0eac7b67461ede0ead5f1e68dc12cdef1fe75d2
Walter Michel Raja <85942041+waltermichelraja@users.noreply.github.com>
2024-11-16 09:22:46 +0530

    patch update 0.2

--- src/core/exceptions.py
+++
@@ -1,9 +0,0 @@
-class LayoutIndexError(IndexError):
-    msg="index out of bounds. "
-    def __init__(self, message=msg):
-        super().__init__(message)
-
-class LayoutAttributeError(AttributeError):
-    msg="cannot modify attribute {}."
-    def __init__(self, message=msg):
-        super().__init__(message)
