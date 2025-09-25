e9ebbc34c6d4c0c7bacbfe8d4ef0a498f1b1ea0c
waltermichelraja <waltermichelraja@gmail.com>
2025-09-25 18:52:01 +0530

    refactor: layout schema

--- src/core/layout_linear.py
+++ src/core/layout_linear.py
@@ -2,36 +2,36 @@ import inspect, sys
 from .exceptions import *
 
 class LinearLayout:
-    def __init__(self) -> None:
+    def __init__(self)->None:
         self.head=None
         self.tail=None
         self.length=0
     
     def __setattr__(self, key, value):
-        module = sys.modules[self.__module__]
-        #core = sys.modules["mercury.src.core.layout"]
+        module=sys.modules[self.__module__]
+        #core=sys.modules["mercury.src.core.layout"]
         currentframe=inspect.getmodule(inspect.currentframe().f_back)
         if module is not None and (module != currentframe):
             if key in self.__dict__:
                 raise LayoutAttributeError(LayoutAttributeError.msg.format(key))
         super().__setattr__(key, value)
 
-    def __getitem__(self, index) -> int:
+    def __getitem__(self, index)->int:
         if not self.head or index >= self.length or index < -self.length:
             raise LayoutIndexError()
-        if index >= 0:
-            current = self.head
-            while current and current.index < index:
-                current = current.next_node
-            if current and current.index == index:
+        if index>=0:
+            current=self.head
+            while current and current.index<index:
+                current=current.next_node
+            if current and current.index==index:
                 return current.data
         else:
-            current = self.tail
-            reverse_index = -1
-            while current and reverse_index > index:
-                current = current.prev_node
-                reverse_index -= 1
-            if current and reverse_index == index:
+            current=self.tail
+            reverse_index=-1
+            while current and reverse_index>index:
+                current=current.prev_node
+                reverse_index-=1
+            if current and reverse_index==index:
                 return current.data
         raise LayoutIndexError()
 
@@ -39,24 +39,24 @@ class LinearLayout:
         if not self.head or index >= self.length or index < -self.length:
             raise LayoutIndexError()
         if index >= 0:
-            current = self.head
+            current=self.head
             while current and current.index < index:
-                current = current.next_node
-            if current and current.index == index:
-                current.data = value
+                current=current.next_node
+            if current and current.index==index:
+                current.data=value
                 return
         else:
-            current = self.tail
-            reverse_index = -1
+            current=self.tail
+            reverse_index=-1
             while current and reverse_index > index:
-                current = current.prev_node
+                current=current.prev_node
                 reverse_index -= 1
-            if current and reverse_index == index:
-                current.data = value
+            if current and reverse_index==index:
+                current.data=value
                 return
         raise LayoutIndexError()
       
-    def __repr__(self) -> str:
+    def __repr__(self)->str:
         nodes=[]
         current=self.head
         if not current:
@@ -68,15 +68,15 @@ class LinearLayout:
         return "{0} < {1} >".format(self.__class__.__name__, " ; ".join(nodes))
 
     class Node():
-        def __init__(self, data) -> None:
+        def __init__(self, data)->None:
             self.data=data
             self.index=None
             self.next_node=None
             self.prev_node=None
 
         def __setattr__(self, key, value):
-            currentmodule = sys.modules[self.__module__]
-            currentframe = inspect.getmodule(inspect.currentframe().f_back)
+            currentmodule=sys.modules[self.__module__]
+            currentframe=inspect.getmodule(inspect.currentframe().f_back)
             if key=="data":
                 super().__setattr__(key, value)
                 return
@@ -89,53 +89,45 @@ class LinearLayout:
             else:
                 raise LayoutAttributeError(LayoutAttributeError.msg.format(key))
             
-        def __repr__(self) -> str:
+        def __repr__(self)->str:
             return "{0} < data: {1} >".format(self.__class__.__name__, self.data)
 
     class Manager:
-        def __init__(self) -> None:
-            pass
+        @staticmethod
+        def is_empty(layout: "LinearLayout")->bool:
+            return layout.head is None
 
-        def is_empty(self):
-            return self.head is None
-        
-        def is_node(self, data):
-            if type(data)==type(LinearLayout.Node(data)):
-                return True
-            else:
-                return False
-        
-        def __update_indices_after_remove(self, start_node):
-            current = start_node
-            while current:
-                current.index -= 1
-                current = current.next_node
+        @staticmethod
+        def is_node(data)->bool:
+            return isinstance(data, LinearLayout.Node)
 
-        def __update_indices_after_insert(self, start_node):
-            current = start_node
+        @staticmethod
+        def _update_indices_after_remove(start_node: "LinearLayout.Node")->None:
+            current=start_node
             while current:
-                current.index += 1
-                current = current.next_node
-
-    def fetchindex(self, data) -> int:
-        if not self.head:
-            return 
-        else:
-            current=self.head
-            while current:
-                if current.data==data:
-                    return current.index
+                current.index-=1
                 current=current.next_node
-            return
-        
-"""    def fetchnode(self, index):
-        if not self.head:
-            return
-        else:
-            current=self.head
-            while current.index==index:
-                return current.data
+
+        @staticmethod
+        def _update_indices_after_insert(start_node: "LinearLayout.Node")->None:
+            current=start_node
+            while current:
+                current.index+=1
+                current=current.next_node
+
+    def index(self, data)->int | None:
+        current=self.head
+        while current:
+            if current.data==data:
+                return current.index
             current=current.next_node
-        return
-"""
+        return None
+        
+    def node(self, index):
+        current=self.head
+        while current:
+            if current.index==index:
+                return current
+            current=current.next_node
+        return None
 
