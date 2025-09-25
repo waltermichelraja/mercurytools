491ee60ceb9c1d557d1ddeb2050fa00870d750c3
waltermichelraja <waltermichelraja@gmail.com>
2025-09-25 20:14:32 +0530

    refactor: minor restructure
    perf: method optimizations

--- src/type/linked_list.py
+++ src/type/linked_list.py
@@ -3,77 +3,68 @@ from ..core.layout_linear import LinearLayout
 class LinkedList(LinearLayout):
     def __init__(self)->None:
         super().__init__()
-        
+
     def append(self, data)->None:
-        if super().Manager().is_node(data):
+        if super().Manager.is_node(data):
             data=data.data
         new_node=super().Node(data)
         new_node.index=self.length
-        if self.head is None:
+        if not self.head:
             self.head=self.tail=new_node
         else:
             new_node.prev_node=self.tail
             self.tail.next_node=new_node
             self.tail=new_node
         self.length+=1
-        return
 
     def prepend(self, data)->None:
-        if super().Manager().is_node(data):
+        if super().Manager.is_node(data):
             data=data.data
         new_node=super().Node(data)
         new_node.index=0
-
-        if self.head is None:
+        if not self.head:
             self.head=self.tail=new_node
         else:
             new_node.next_node=self.head
             self.head.prev_node=new_node
             self.head=new_node
-        super().Manager()._update_indices_after_insert(new_node.next_node)
+            super().Manager._update_indices_after_insert(new_node.next_node)
         self.length+=1
-        return
 
-    def insert(self, data, index=None)->None:
-        if index is not None and index > self.length:
-            raise IndexError("index out of bounds")
+    def insert(self, data, index: int | None=None)->None:
         if index is None:
             index=self.length
-        if super().Manager().is_node(data):
+        if index>self.length or index < 0:
+            raise IndexError("index out of bounds")
+        if super().Manager.is_node(data):
             data=data.data
-        new_node=super().Node(data)
-        new_node.index=index
-        if self.head is None:
-            self.head=self.tail=new_node
-        elif index==self.length:
+        if index==self.length:
             self.append(data)
             return
-        elif index==0:
+        if index==0:
             self.prepend(data)
             return
-        else:
-            current=self.head
-            while current and current.index < index:
-                current=current.next_node
-            new_node.prev_node=current.prev_node
-            new_node.next_node=current
-            if current.prev_node:
-                current.prev_node.next_node=new_node
-            current.prev_node=new_node
-            if index==0:
-                self.head=new_node
-            super().Manager()._update_indices_after_insert(new_node.next_node)
+        current=self.head
+        while current and current.index < index:
+            current=current.next_node
+        new_node=super().Node(data)
+        new_node.index=index
+        new_node.prev_node=current.prev_node
+        new_node.next_node=current
+        if current.prev_node:
+            current.prev_node.next_node=new_node
+        current.prev_node=new_node
+        super().Manager._update_indices_after_insert(new_node.next_node)
         self.length+=1
-        return
 
     def remove(self, data, start_index=0):
         if start_index<0 or start_index>=self.length:
             raise IndexError("index out of bounds")
         current=self.head
-        length=0
-        while current and length<start_index:
+        i=0
+        while current and i<start_index:
             current=current.next_node
-            length+=1
+            i+=1
         while current:
             if current.data==data:
                 if current.prev_node:
@@ -84,9 +75,9 @@ class LinkedList(LinearLayout):
                     current.next_node.prev_node=current.prev_node
                 else:
                     self.tail=current.prev_node
-                super().Manager()._update_indices_after_remove(current.next_node)
+                super().Manager._update_indices_after_remove(current.next_node)
                 self.length-=1
                 return current.data
             current=current.next_node
-            length+=1
-        return None
+            i+=1
+        raise ValueError(f"data: {data} not found")
--- src/type/stack.py
+++ src/type/stack.py
@@ -3,31 +3,30 @@ from ..core.layout_linear import LinearLayout
 class Stack(LinearLayout):
     def __init__(self)->None:
         super().__init__()
-                
+
     def push(self, data)->None:
-        if super().Manager().is_node(data):
+        if super().Manager.is_node(data):
             data=data.data
         new_node=super().Node(data)
         new_node.index=self.length
-        if self.head is None:
-            self.head=new_node
-            self.tail=new_node
+        if not self.head:
+            self.head=self.tail=new_node
         else:
             new_node.prev_node=self.tail
             self.tail.next_node=new_node
             self.tail=new_node
         self.length+=1
-        return
-    
+
     def pop(self):
-        if self.head is None:
+        if not self.head:
             raise IndexError("pop from an empty stack")
         pop_data=self.tail.data
         if self.head==self.tail:
-            self.head=None
-            self.tail=None
+            self.head=self.tail=None
         else:
+            old_tail=self.tail
             self.tail=self.tail.prev_node
             self.tail.next_node=None
+            super().Manager._update_indices_after_remove(self.tail.next_node)
         self.length-=1
         return pop_data
