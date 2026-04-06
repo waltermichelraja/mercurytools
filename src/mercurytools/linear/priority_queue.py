d7fca53594bc4c93f48273cdb32618d1735452a5
waltermichelraja <waltermichelraja@gmail.com>
2026-04-06 22:06:23 +0530

    feat(linear): PriorityQueue with optional priority support feat(linear): maintained backward compatibility with value-based (min-heap) usage

--- src/mercurytools/linear/priority_queue.py
+++ src/mercurytools/linear/priority_queue.py
@@ -1,29 +1,51 @@
 class PriorityQueue:
     def __init__(self):
         self._data=[]
+        self._uses_priority=None
+        self._counter=0
 
     def __len__(self):
         return len(self._data)
 
     def __repr__(self):
-        return f"PriorityQueue({self._data})"
+        return f"PriorityQueue({[item[2] for item in self._data]})"
 
-    def push(self,value):
-        self._data.append(value)
+    def push(self,value,priority=None):
+        if priority is not None:
+            if self._uses_priority is False:
+                raise ValueError("cannot mix priority and non-priority values")
+            self._uses_priority=True
+        else:
+            if self._uses_priority is True:
+                raise ValueError("cannot mix priority and non-priority values")
+            self._uses_priority=False
+        if priority is None:
+            entry=(value,self._counter,value)
+        else:
+            entry=(priority,self._counter,value)
+        self._counter+=1
+        self._data.append(entry)
         self._heapify_up(len(self._data)-1)
 
     def pop(self):
         if not self._data:
             raise IndexError("pop from empty priority queue")
         self._swap(0,len(self._data)-1)
-        value=self._data.pop()
+        _,_,value=self._data.pop()
         self._heapify_down(0)
         return value
 
     def peek(self):
         if not self._data:
             return None
-        return self._data[0]
+        return self._data[0][2]
+
+    def to_list(self):
+        return [item[2] for item in self._data]
+
+
+    def _priority(self,item):
+        return item[0]
 
     def _parent(self,i):
         return (i-1)//2
@@ -40,11 +62,14 @@ class PriorityQueue:
     def _heapify_up(self,i):
         while i>0:
             p=self._parent(i)
-            if self._data[i]<self._data[p]:
-                self._swap(i,p)
-                i=p
-            else:
-                break
+            try:
+                if self._priority(self._data[i])<self._priority(self._data[p]):
+                    self._swap(i,p)
+                    i=p
+                else:
+                    break
+            except TypeError:
+                raise TypeError("values are not comparable for priority queue")
 
     def _heapify_down(self,i):
         size=len(self._data)
@@ -52,10 +77,13 @@ class PriorityQueue:
             left=self._left(i)
             right=self._right(i)
             smallest=i
-            if left<size and self._data[left]<self._data[smallest]:
-                smallest=left
-            if right<size and self._data[right]<self._data[smallest]:
-                smallest=right
+            try:
+                if left<size and self._priority(self._data[left])<self._priority(self._data[smallest]):
+                    smallest=left
+                if right<size and self._priority(self._data[right])<self._priority(self._data[smallest]):
+                    smallest=right
+            except TypeError:
+                raise TypeError("values are not comparable for priority queue")
             if smallest!=i:
                 self._swap(i,smallest)
                 i=smallest
