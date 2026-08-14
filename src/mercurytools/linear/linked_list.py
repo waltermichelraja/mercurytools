e4bd823ee219bbe016e4f9a68dd8c1052baa5178
Walter Michel Raja <waltermichelraja@gmail.com>
2026-08-14 23:20:12 +0530

    docs: docstrings to linear/

--- src/mercurytools/linear/linked_list.py
+++ src/mercurytools/linear/linked_list.py
@@ -1,16 +1,24 @@
+"""doubly-linked list."""
+
+
 from ..core.base_linear import LinearBase
 from ..core.nodes import LinearNode as Node
 from ..core.exceptions import IndexOutOfBoundsError,ValueNotFoundError
 
 
 class LinkedList(LinearBase):
+    """a doubly-linked list supporting O(1) append/prepend and indexed insert/remove."""
+
     def append(self,data):
+        """add data to the end: O(1)."""
         self._append_node(Node(data))
 
     def prepend(self,data):
+        """add data to the start: O(1)."""
         self._prepend_node(Node(data))
 
     def insert(self,index,data):
+        """insert data before the element currently at index."""
         if index<0 or index>self._size:
             raise IndexOutOfBoundsError()
         if index==0:
@@ -30,6 +38,7 @@ class LinkedList(LinearBase):
         self._set_size(self._size+1)
 
     def remove(self,value):
+        """remove and return the first element equal to value."""
         current=self._head
         while current:
             if current.data==value:
