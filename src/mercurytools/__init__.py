7784be72cb314e1def0a73f2fd6cdb4bb2d17def
Walter Michel Raja <waltermichelraja@gmail.com>
2026-08-30 15:42:01 +0530

    feat(disjoint_set): DisjointSet [union-find] with path compression

--- src/mercurytools/__init__.py
+++ src/mercurytools/__init__.py
@@ -1,10 +1,11 @@
 """mercurytools: a lightweight, consistent data structures library.
 
 submodules:
-    linear -- LinkedList, Stack, Deque, PriorityQueue
-    tree   -- BinaryTree, BinarySearchTree, AVLTree, Trie
-    graph  -- Graph [adjacency list, BFS/DFS]
-    utils  -- LRUCache
+    linear       -- LinkedList, Stack, Deque, PriorityQueue
+    tree         -- BinaryTree, BinarySearchTree, AVLTree, Trie
+    graph        -- Graph [adjacency list, BFS/DFS]
+    disjoint_set -- DisjointSet [union-find]
+    utils        -- LRUCache
 """
 
 
@@ -12,6 +13,7 @@ from . import linear
 from . import utils
 from . import tree
 from . import graph
+from . import disjoint_set
 from .__version__ import __version__
 
-__all__=["linear","utils","tree","graph","__version__"]
+__all__=["linear","utils","tree","graph","disjoint_set","__version__"]
